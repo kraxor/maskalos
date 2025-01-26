2890112858fbc42af95e81473d63ee6247abaa76
Bence Balint <bence.balint@sonofabit.ch>
2025-01-26 17:53:45 +0000

    move player upwards

--- main.py
+++ main.py
@@ -6,17 +6,34 @@ pygame.init()
 screen_dimensions = (960, 540)
 window = pygame.display.set_mode(screen_dimensions)
 
-player = pygame.Rect(screen_dimensions[0] / 2, screen_dimensions[1] / 2, 10, 10)
+player_x = screen_dimensions[0] / 2
+player_y = screen_dimensions[1] / 2
+
 player_color = pygame.Color(255, 0, 0)
 
 running = True
 while running:
+    # handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("QUIT event received, leaving game loop...")
             running = False
 
-    pygame.draw.rect(window, player_color, player)
+    # handle key presses
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_y -= 0.1
+
+    # create player rect
+    player_rect = pygame.Rect(player_x, player_y, 10, 10)
+
+    # clear screen
+    window.fill(pygame.Color(0, 0, 0))
+
+    # draw player rect
+    pygame.draw.rect(window, player_color, player_rect)
+
+    # flip display (apply changes)
     pygame.display.flip()
 
 print("Cleaning up...")
