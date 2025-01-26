03ee6787f8e7a9d94e1938fe36e12ed16f97227d
Bence Balint <bence.balint@sonofabit.ch>
2025-01-26 18:11:57 +0000

    add npc

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
-import time
 
 pygame.init()
 
 # game settings
 speed = 2
 player_color = pygame.Color(255, 0, 0)
+npc_color = pygame.Color(0, 0, 255)
 screen_dimensions = (960, 540)
 frame_rate = 60 # FPS (frames per second)
+character_width = 40
+character_height = 40
 
 # create screen
 window = pygame.display.set_mode(screen_dimensions)
@@ -18,6 +20,10 @@ clock = pygame.time.Clock()
 # set player starting coordinates
 player_x = screen_dimensions[0] / 2
 player_y = screen_dimensions[1] / 2
+
+# set NPC starting coordinates
+npc_x = 200
+npc_y = 200
 
 running = True
 while running:
@@ -38,16 +44,18 @@ while running:
     elif keys[pygame.K_d]:
         player_x = player_x + speed
 
-    print("player location: %f,%f" % (player_x, player_y))
+    # print("player location: %f,%f" % (player_x, player_y))
 
-    # create player rect
-    player_rect = pygame.Rect(player_x, player_y, 40, 40)
+    # create rects for characters
+    player_rect = pygame.Rect(player_x, player_y, character_width, character_height)
+    npc_rect = pygame.Rect(npc_x, npc_y, character_width, character_height)
 
     # clear screen
     window.fill(pygame.Color(0, 0, 0))
 
-    # draw player rect
+    # draw character rects
     pygame.draw.rect(window, player_color, player_rect)
+    pygame.draw.rect(window, npc_color, npc_rect)
 
     # flip display (apply changes)
     pygame.display.flip()
